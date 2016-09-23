0caf606e1308726a7bb7362978089ac8343686f1
Ted Callahan <CCallahanIV@gmail.com>
2016-09-22 19:57:05 -0700

    Trying to fix score functionality

--- js/jeopardy.js
+++ js/jeopardy.js
@@ -1,5 +1,7 @@
 //This is the main javascript file for the functionality of the jeopardy app.
 
+localStorage.clear();
+
 function writeGameBoard(){
   if(!localStorage.getItem("boardState")){
     var boardState = [
@@ -22,7 +24,10 @@ function writeGameBoard(){
 
 function newUser(){
   var userName = prompt("Please enter your name: ");
-  localStorage.setItem("user", userName)
+  localStorage.setItem("user", userName);
+
+  var elUserName = document.getElementById("userName");
+  elUserName.textContent = userName;
 };
 
 function newGame(){
@@ -41,46 +46,76 @@ function newGame(){
   var catArray = [0,1,2,3];
   localStorage.setItem("catArray", JSON.stringify(catArray));
 
+  var dailyDouble = "12";
+  localStorage.setItem("dailyDouble", dailyDouble);
+
   writeGameBoard();
 };
 
-function loadAnswer(e){
+//Main game function - asking and answering questions
+function game(e){
   var id = e.target.getAttribute('id');
   var cat = categories[Number(id.charAt(0))];
-  var aBox = document.getElementsByClassName("answerBox")[0];
   var ans = "a"+(Number(id.charAt(1))+1);
-  console.log(ans);
 
+  //Clear Game Board Button and display answer
   clearBtn(e);
-  aBox.innerHTML=cat[ans].answer;
 
+  //Display Answer in answer box
+  var elaBox = document.getElementsByClassName("answerBox")[0];
+  elaBox.innerHTML=cat[ans].answer;
+
+  //Display Questions
   for(var i=0; i<3; i++){
     var ind = "q"+(i+1);
     var question = document.getElementById(ind);
     var qtext = cat[ans][ind];
     question.innerHTML=qtext;
   }
+
+  //Create event listener for clicking on question box.
+  elqList = document.getElementById("qList");
+  elqList.addEventListener('click', function(choice){
+    var points = 0;
+    var choiceID = choice.target.getAttribute('id');
+    if(choiceID == cat[ans].correct){
+      console.log(choiceID + "correct!");
+      points = cat[ans].points;
+    } else {
+      console.log(choiceID + "incorrect");
+      points = -(cat[ans].points);
+    }
+    updateScore(points);
+  }, false);
+
 };
 
-function checkQuestion(e){
-  var id = e.target.getAttribute('id');
-};
-
+//Function to clear game board tiles
 function clearBtn(e){
   var id = e.target.getAttribute("id");
   e.target.textContent=" ";
   var boardState = JSON.parse(localStorage.getItem("boardState"));
-  boardState[Number(id.charAt(0))][Number(id.charAt(1))] = "";
+  boardState[Number(id.charAt(1))][Number(id.charAt(0))] = "";
   localStorage.setItem("boardState", JSON.stringify(boardState));
+};
+
+//Function to update user score, store in local storage.
+function updateScore(points){
+  var score = parseInt(localStorage.getItem("userScore"));
+  console.log("score before:" + score);
+
+  score += points;
+  localStorage.setItem("userScore", score);
+  console.log("score after:" + score);
+
+  elUserScore = document.getElementById("userScore");
+  elUserScore.textContent = score;
 };
 
 window.addEventListener('load', writeGameBoard, false);
 
-var elQBtn = document.getElementById("qList");
-elQBtn.addEventListener('click', function(e) { checkQuestion(e) }, false);
-
 var elBtn = document.getElementById("gameBoard")
-elBtn.addEventListener('click', function(e) { loadAnswer(e) }, false);
+elBtn.addEventListener('click', function(e) { game(e) }, false);
 
 var elNewGame = document.getElementById("newGame");
 elNewGame.addEventListener('click', newGame,false);
