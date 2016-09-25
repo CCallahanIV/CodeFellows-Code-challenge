daa33ba8a7360b3ad662514a81df21bb53f0f0d0
Ted Callahan <CCallahanIV@gmail.com>
2016-09-25 10:07:36 -0700

    updated structure to OOP-ish.  Set up page refresh abilities

--- js/jeopardy.js
+++ js/jeopardy.js
@@ -1,141 +1,217 @@
 
 //This is the main javascript file for the functionality of the jeopardy app.
 
-localStorage.clear();
+//localStorage.clear();
+
+//This function writes the gameboard on page load and checks to see if localstorage variables exist from before page refresh.
+
+var board = {
+  gameState: {
+    userName : "",
+    userScore: 0,
+    boardState: [[200,200,200,200],
+                  [600,600,600,600],
+                  [1000,1000,1000,1000]],
+    catArray: [],
+    libraryMap: [],
+    selectedQ: "",
+    isJAnswerable: true,
+    isJQuestionable: false
+  },
+
+  storeGameState: function(){
+    localStorage.setItem("gameState", JSON.stringify(this.gameState));
+  },
+
+  updateGameState: function(key, value){
+    this.gameState.key = value;
+  },
+
+  loadGameStateItem: function(key){
+    var gameState = JSON.parse(localStorage.getItem("gameState"));
+    return gameState.key;
+  },
+
+  loadGameState: function(){
+    if(localStorage.getItem("gameState") !== null){
+      this.gameState = JSON.parse(localStorage.getItem("gameState"));
+      console.log("Loaded from local storage.");
+    }
+  },
+
+  genCatArray: function(){
+    function randomNum(min, max){
+      return Math.round((max-min) * Math.random() + min);
+    }
+    function numFound(rNum, rArray){
+      for (var i=0; i<rArray.length; i++){
+        if(rNum==rArray[i]){
+          return (-1);
+        }
+      }
+      return rNum;
+    }
+    function uniqueArray(l, min, max){
+      var temp, rArray = new Array;
+      for (var i=0; i<l; i++){
+        while((temp = numFound(randomNum(min, max),rArray))==-1);
+        rArray[i]=temp;
+      }
+      return rArray;
+    }
+    this.gameState.catArray=uniqueArray(5,0,7);
+  },
+
+  writeUserName: function(){
+    var elUserName = document.getElementById("userName");
+    elUserName.textContent = this.gameState.userName;
+  },
+
+  writeUserScore: function(){
+    var elUserScore = document.getElementById("userScore");
+    elUserScore.textContent = this.gameState.userScore;
+  },
+
+  writeAnswerAndQs: function(){
+    var elaBox = document.getElementsByClassName("answerBox")[0];
+    if(!(this.gameState.libraryMap.length > 0)){
+      elaBox.innerHTML = "Welcome to Joepardy-Lite!"
+      return;
+    }
+
+    var cat = this.gameState.libraryMap[0];
+    var ans = "a"+(Number(this.gameState.libraryMap[1]));
+
+    elaBox.innerHTML = categories[this.gameState.catArray[cat]][ans].answer;
+    for(var i=0; i<3; i++){
+      var ind = "q"+i;
+      var question = document.getElementById(ind);
+      var qtext = categories[this.gameState.catArray[cat]][ans][ind];
+      question.innerHTML=qtext;
+      question.setAttribute('class','questionBox');
+    }
+  },
+
+  writeCats: function(){
+    if(!(this.gameState.catArray.length>0)){
+      return;
+    }
+
+    var elCategories = document.getElementsByClassName("category");
+    for(var k=0; k<4; k++){
+      elCategories[k].textContent = categories[this.gameState.catArray[k]].cat;
+    }
+  },
+
+  writeBoard: function(){
+    for(var i=0;i<3;i++){
+      for(var j=0;j<4;j++){
+        var id = j.toString()+i.toString();
+        var elGameBtn = document.getElementById(id);
+        elGameBtn.textContent=this.gameState.boardState[i][j];
+      }
+    }
+  },
+
+  checkIfSelected: function(id){
+    if(this.gameState[Number(id.charAt(1))][Number(id.charAt(0))]===""){
+      window.alert("Already chose this one, please select another.");
+      return true;
+    }
+  },
+
+  checkAnswer: function(){
+    console.log(this.gameState.libraryMap);
+    var cat = categories[this.gameState.catArray[this.gameState.libraryMap[0]]];
+    var ans = "a"+(Number(this.gameState.libraryMap[1]));
+    var choiceID = "q"+(Number(this.gameState.libraryMap[2]));
+
+    var points = 0;
+    var correct;
+
+    if(choiceID == cat[ans].correct){
+      points = cat[ans].points;
+      correct = true;
+    } else {
+      points = -(cat[ans].points);
+      correct = false;
+    }
+    this.gameState.userScore += points;
+    switchQClass(correct, choiceID);
+  }
+
+};
+
 
 function writeGameBoard(){
-  if(!localStorage.getItem("boardState")){
-    var boardState = [
-      ["","","",""],
-      ["","","",""],
-      ["","","",""]];
-    localStorage.setItem("boardState", JSON.stringify(boardState));
-  }
-  boardState = JSON.parse(localStorage.getItem("boardState"));
-  console.log(boardState);
 
-  for(var i=0;i<3;i++){
-    for(var j=0;j<4;j++){
-      var id = j.toString()+i.toString();
-      var elGameBtn = document.getElementById(id);
-      elGameBtn.textContent=boardState[i][j];
-    }
-  }
+  board.loadGameState();
+  board.writeUserName();
+  board.writeUserScore();
+  board.writeCats();
+  board.writeBoard();
+  board.writeAnswerAndQs();
+
 };
 
 function newUser(){
-  var userName = prompt("Please enter your name: ");
-  localStorage.setItem("user", userName);
-
-  var elUserName = document.getElementById("userName");
-  elUserName.textContent = userName;
+  board.gameState.userName = prompt("Please enter your name: ");
+  board.writeUserName();
 };
 
 function newGame(){
   localStorage.clear();
+
+  board.genCatArray();
   newUser();
-  //Declare variable to track state of game board.  Must be stored locally to ensure game board state survives page refresh.
-  var boardState = [
-    [200,200,200,200],
-    [600,600,600,600],
-    [1000,1000,1000,1000]];
-  localStorage.setItem("boardState", JSON.stringify(boardState));
-
-  var userScore = 0;
-  localStorage.setItem("userScore", userScore);
-
-  var catArray = [0,1,2,3];
-  localStorage.setItem("catArray", JSON.stringify(catArray));
-
-  var dailyDouble = "12";
-  localStorage.setItem("dailyDouble", dailyDouble);
-
   writeGameBoard();
+  console.log(board.gameState.catArray);
+
 };
 
 //write answer and questions
-function game(e){
+function selectAnswer(e){
+
+  if(board.gameState.isJAnswerable === false){
+    window.alert("Please select a question!");
+    return;
+  }
   var id = e.target.getAttribute('id');
-  var cat = categories[Number(id.charAt(0))];
-  var ans = "a"+(Number(id.charAt(1))+1);
-  //Display Answer in answer box
-  var elaBox = document.getElementsByClassName("answerBox")[0];
 
-  if(elaBox.getAttribute("isJAnswerable")==='false'){
-    window.alert("Please select a game tile");
-    return;
-  }
-  var boardState = JSON.parse(localStorage.getItem("boardState"));
-  if(boardState[Number(id.charAt(1))][Number(id.charAt(0))]===""){
-    window.alert("Already chose this one, please select another.");
-    return;
-  }
+  board.gameState.libraryMap=[];
+  board.gameState.libraryMap[0]=Number(id.charAt(0));
+  board.gameState.libraryMap[1]=Number(id.charAt(1));
 
-  //Clear Game Board Button and display answer
+  board.writeAnswerAndQs();
   clearBtn(e);
 
-
-  elaBox.innerHTML=cat[ans].answer;
-	elaBox.setAttribute('data',id);
-
-  //Display Questions
-  for(var i=0; i<3; i++){
-    var ind = "q"+(i+1);
-    var question = document.getElementById(ind);
-    var qtext = cat[ans][ind];
-    question.innerHTML=qtext;
-    question.setAttribute('class','questionBox');
-  }
-  elaBox.setAttribute("isJQuestionable","true");
-  elaBox.setAttribute("isJAnswerable","false");
+  board.gameState.isJQuestionable=true;
+  board.gameState.isJAnswerable=false;
 };
 
 //Function to clear game board tiles
 function clearBtn(e){
   var id = e.target.getAttribute("id");
+  if(id[0]==="c"){
+    return;
+  }
   e.target.textContent=" ";
-  var boardState = JSON.parse(localStorage.getItem("boardState"));
-  boardState[Number(id.charAt(1))][Number(id.charAt(0))] = "";
-  localStorage.setItem("boardState", JSON.stringify(boardState));
+  board.gameState.boardState[Number(id.charAt(1))][Number(id.charAt(0))] = "";
 };
 
-//Function to update user score, store in local storage.
-function updateScore(points){
-  var score = parseInt(localStorage.getItem("userScore"));
-  console.log("score before:" + score);
-
-  score += points;
-  localStorage.setItem("userScore", score);
-  console.log("score after:" + score);
-
-  elUserScore = document.getElementById("userScore");
-  elUserScore.textContent = score;
-};
-
-function checkAnswer(evt){
-	var elaBox = document.getElementsByClassName('answerBox')[0];
-	var id = elaBox.getAttribute('data');
-
-  if(elaBox.getAttribute("isJQuestionable")==='false'){
-    window.alert("Please select a question!");
+function selectQuestion(evt){
+  if(board.gameState.isJQuestionable===false){
+    window.alert('Please select an Answer');
     return;
   }
 
 	var choiceID = evt.target.getAttribute('id');
-	var cat = categories[Number(id.charAt(0))];
-	var ans = "a"+(Number(id.charAt(1))+1);
-	var points = 0;
-
-	if(choiceID == cat[ans].correct){
-		points = cat[ans].points;
-		switchQClass(true, choiceID);
-	} else {
-		points = -(cat[ans].points);
-		switchQClass(false, choiceID);
-	}
-	updateScore(points);
-  elaBox.setAttribute("isJAnswerable","true");
-  elaBox.setAttribute("isJQuestionable","false");
+  console.log(choiceID);
+  board.gameState.libraryMap[2] = Number(choiceID.charAt(1));
+  board.checkAnswer();
+  board.gameState.isJAnswerable = true;
+  board.gameState.isJQuestionable = false;
+  board.writeUserScore();
 };
 
 function switchQClass(correct, choiceID){
@@ -147,13 +223,19 @@ function switchQClass(correct, choiceID){
 	}
 };
 
+function storeState(){
+  board.storeGameState();
+}
+
 window.addEventListener('load', writeGameBoard, false);
 
+window.addEventListener('beforeunload', storeState, false);
+
 var elqList = document.getElementById("qList");
-elqList.addEventListener('click', function(e) { checkAnswer(e)}, false);
+elqList.addEventListener('click', function(evt) { selectQuestion(evt)}, false);
 
 var elBtn = document.getElementById("gameBoard")
-elBtn.addEventListener('click', function(evt) { game(evt) }, false);
+elBtn.addEventListener('click', function(e) { selectAnswer(e) }, false);
 
 var elNewGame = document.getElementById("newGame");
 elNewGame.addEventListener('click', newGame,false);
--- js/library.js
+++ js/library.js
@@ -2,215 +2,215 @@
 
 var sports = {
   "cat":"Sports",
-  "a1": {
+  "a0": {
     "answer":"The most recent winner of the Triple Crown.",
-    "q1":"Who is Secretariat?",
-    "q2":"Who is Seattle Slew?",
-    "q3":"Who is American Pharoah?",
-    "correct":"q3",
+    "q0":"Who is Secretariat?",
+    "q1":"Who is Seattle Slew?",
+    "q2":"Who is American Pharoah?",
+    "correct":"q2",
     "points":200},
-  "a2": {
+  "a1": {
     "answer":"This city will host the 2020 Summer Olympics.",
-    "q1":"What is Tokyo?",
-    "q2":"What is Buenos Aires?",
-    "q3":"What is Los Angeles?",
-    "correct":"q1",
+    "q0":"What is Tokyo?",
+    "q1":"What is Buenos Aires?",
+    "q2":"What is Los Angeles?",
+    "correct":"q0",
     "points":600},
-  "a3": {
+  "a2": {
     "answer":"The Rose Bowl is located in this city.",
-    "q1":"What is Pasadena, CA?",
-    "q2":"What is San Francisco, CA?",
-    "q3":"What is Portland, OR?",
-    "correct":"q1",
+    "q0":"What is Pasadena, CA?",
+    "q1":"What is San Francisco, CA?",
+    "q2":"What is Portland, OR?",
+    "correct":"q0",
     "points":1000}
 };
 
 var entertainment = {
   "cat":"Entertainment",
-  "a1": {
+  "a0": {
     "answer":"This movie won the Academy Award for Best Drama in 2016.",
-    "q1":"What is <em>The Martian</em>",
-    "q2":"What is <em>The Revenant</em>",
-    "q3":"What is <em>Spotlight</em>",
-    "correct":"q3",
+    "q0":"What is <em>The Martian</em>?",
+    "q1":"What is <em>The Revenant</em>?",
+    "q2":"What is <em>Spotlight</em>?",
+    "correct":"q2",
     "points":200},
-  "a2":{
+  "a1":{
     "answer":"This British, Grammy-winning singer/songwriter's first three albums were named after her age.",
-    "q1":"Who is Amy Winehouse.",
-    "q2":"Who is Adele",
-    "q3":"Who is Ellie Goulding",
-    "correct":"q2",
+    "q0":"Who is Amy Winehouse?",
+    "q1":"Who is Adele?",
+    "q2":"Who is Ellie Goulding?",
+    "correct":"q1",
     "points":600},
-  "a3": {
+  "a2": {
     "answer":"This sitcom follows the lives of six twenty-somethings living in New York that frequent a local coffee shop.",
-    "q1":"What is <em>How I Met your Mother</em>",
-    "q2":"What is <em>Friends</em>",
-    "q3":"What is <em>Third Rock from the Sun</em>",
-    "correct":"q2",
+    "q0":"What is <em>How I Met your Mother</em>?",
+    "q1":"What is <em>Friends</em>?",
+    "q2":"What is <em>Third Rock from the Sun</em>?",
+    "correct":"q1",
     "points":1000
   }
 };
 
 var literature = {
   "cat":"Literature",
-  "a1":{
-    "answer":"This author penned children/'s novels such as <em>Matilda</em>, <em>The BFG</em>, and <em>Charlie and the Chocolate Factory</em>.",
-    "q1":"Who is Roald Dahl?",
-    "q2":"Who is Beverly Cleary?",
-    "q3":"Who is Judy Blume?",
-    "correct":"q1",
+  "a0":{
+    "answer":"This author penned children\'s novels such as <em>Matilda</em>, <em>The BFG</em>, and <em>Charlie and the Chocolate Factory</em>.",
+    "q0":"Who is Roald Dahl?",
+    "q1":"Who is Beverly Cleary?",
+    "q2":"Who is Judy Blume?",
+    "correct":"q0",
     "points":200,
   },
-  "a2":{
+  "a1":{
     "answer":"Jane Austen/'s <em>Pride and Prejudice</em> follows main character Elizabeth Bennett and this elegible bachelor.",
-    "q1":"Who is Mr. Darby?",
-    "q2":"Who is Mr. Darcy?",
-    "q3":"Mr. Darley?",
-    "correct":"q2",
+    "q0":"Who is Mr. Darby?",
+    "q1":"Who is Mr. Darcy?",
+    "q2":"Who is Mr. Darley?",
+    "correct":"q1",
     "points":600
   },
-  "a3":{
+  "a2":{
     "answer":"This F. Scott Fitzgerald novel was made into a movie starring Leonardo DiCaprio in 2013.",
-    "q1":"What is <em>This Side of Paradise</em>?",
-    "q2":"What is <em>The Great Gatsby</em>?",
-    "q3":"What is <em>Tender is the Night</em>?",
-    "correct":"q2",
+    "q0":"What is <em>This Side of Paradise</em>?",
+    "q1":"What is <em>The Great Gatsby</em>?",
+    "q2":"What is <em>Tender is the Night</em>?",
+    "correct":"q1",
     "points":1000
   }
 };
 
 var geography = {
   "cat":"Geography",
-  "a1":{
+  "a0":{
     "answer":"This is the northernmost state capital in the contiguous United States.",
-    "q1":"What is Olympia, WA?",
-    "q2":"What is Helena, MT?",
-    "q3":"What is Augusta, ME?",
-    "correct":"q1",
+    "q0":"What is Olympia, WA?",
+    "q1":"What is Helena, MT?",
+    "q2":"What is Augusta, ME?",
+    "correct":"q0",
     "points":200
   },
-  "a2":{
+  "a1":{
     "answer":"This country forms a /'boot/' in the Mediterranean Sea.",
-    "q1":"What is Greece?",
-    "q2":"What is Italy?",
-    "q3":"what is Croatia?",
-    "correct":"q2",
+    "q0":"What is Greece?",
+    "q1":"What is Italy?",
+    "q2":"what is Croatia?",
+    "correct":"q1",
     "points":600
   },
-  "a3":{
+  "a2":{
     "answer":"This country experiences the most earthquakes per year.",
-    "q1":"What is Indonesia?",
-    "q2":"What is the United States?",
-    "q3":"What is Japan?",
-    "correct":"q1",
+    "q0":"What is Indonesia?",
+    "q1":"What is the United States?",
+    "q2":"What is Japan?",
+    "correct":"q0",
     "points":1000
   }
 };
 var science = {
   "cat":"Science",
-  "a1":{
+  "a0":{
     "answer":"This is the first Noble Gas on the periodic table.",
-    "q1":"What is Hydrogen?",
-    "q2":"What is Helium?",
-    "q3":"What is Lithium",
-    "correct":"q2",
+    "q0":"What is Hydrogen?",
+    "q1":"What is Helium?",
+    "q2":"What is Lithium",
+    "correct":"q1",
     "points":200
   },
-  "a2":{
+  "a1":{
     "answer":"This is the study of bugs.",
-    "q1":"What is Entomology?",
-    "q2":"What is Etymology?",
-    "q3":"What is Zoology?",
-    "correct":"q1",
+    "q0":"What is Entomology?",
+    "q1":"What is Etymology?",
+    "q2":"What is Zoology?",
+    "correct":"q0",
     "points":600
   },
-  "a3":{
+  "a2":{
     "answer":"In Physics, this is the word used to describe the speed of an object in a given direction.",
-    "q1":"What is Trajectory?",
-    "q2":"What is Velocity?",
-    "q3":"What is Loft?",
-    "correct":"q2",
+    "q0":"What is Trajectory?",
+    "q1":"What is Velocity?",
+    "q2":"What is Loft?",
+    "correct":"q1",
     "points":1000
   }
 };
 var art = {
   "cat":"Art",
-  "a1":{
-    "answer":"Any Warhol was a pioneer in this art form.",
-    "q1":"What is Impressionism?",
-    "q2":"What is Realism?",
-    "q3":"What is Pop Art?",
-    "correct":"q3",
+  "a0":{
+    "answer":"Andy Warhol was a pioneer in this art form.",
+    "q0":"What is Impressionism?",
+    "q1":"What is Realism?",
+    "q2":"What is Pop Art?",
+    "correct":"q2",
     "points":200
   },
-  "a2":{
+  "a1":{
     "answer":"This Parisian museum houses one of the premier collections of impressionist art in the World.",
-    "q1":"What is the Pompidou Center?",
-    "q2":"What is the Louvre?",
-    "q3":"What is the Musee D'Orsay?",
-    "correct":"q3",
+    "q0":"What is the Pompidou Center?",
+    "q1":"What is the Louvre?",
+    "q2":"What is the Musee D'Orsay?",
+    "correct":"q2",
     "points":600
   },
-  "a3":{
+  "a2":{
     "answer":"The <em>Nutcracker Suite</em> is a popular Christmastime production most commonly performed in this dance style.",
-    "q1":"What is Ballet?",
-    "q2":"What is Ballroom?",
-    "q3":"What is Jazz?",
-    "correct":"q1",
+    "q0":"What is Ballet?",
+    "q1":"What is Ballroom?",
+    "q2":"What is Jazz?",
+    "correct":"q0",
     "points":1000
   }
 };
 var food = {
   "cat":"Food and Drink",
-  "a1":{
+  "a0":{
     "answer":"This type of fruit is a common cure for scurvy.",
-    "q1":"What is stone fruit?",
-    "q2":"What is citrus fruit?",
-    "q3":"What are berries?",
-    "correct":"q2",
+    "q0":"What is stone fruit?",
+    "q1":"What is citrus fruit?",
+    "q2":"What are berries?",
+    "correct":"q1",
     "points":200
   },
-  "a2":{
+  "a1":{
     "answer":"This is not one of the core ingredients of beer?",
-    "q1":"What is water?",
-    "q2":"What are hops?",
-    "q3":"What is corn?",
-    "correct":"q3",
+    "q0":"What is water?",
+    "q1":"What are hops?",
+    "q2":"What is corn?",
+    "correct":"q2",
     "points":600
   },
-  "a3":{
+  "a2":{
     "answer":"Filet Mignon is a cut of meat from this part of a cow.",
-    "q1":"What is chuck?",
-    "q2":"What is tenderloin?",
-    "q3":"What is flank?",
-    "correct":"q2",
+    "q0":"What is chuck?",
+    "q1":"What is tenderloin?",
+    "q2":"What is flank?",
+    "correct":"q1",
     "points":1000
   }
 };
 var politics = {
   "cat":"Politics",
-  "a1":{
+  "a0":{
     "answer":"Bernie Sanders is the long-serving Senator from this US State.",
-    "q1":"What is Vermont?",
-    "q2":"What is Delaware?",
-    "q3":"What is Rhode Island?",
-    "correct":"q1",
+    "q0":"What is Vermont?",
+    "q1":"What is Delaware?",
+    "q2":"What is Rhode Island?",
+    "correct":"q0",
     "points":200
   },
-  "a2":{
+  "a1":{
     "answer":"This series of treaties ratified by 196 states provides rules for the treatment of prisoners of war, civilians, and soldiers outside of combat.",
-    "q1":"What is the Treaty of Versailles?",
-    "q2":"What is the Geneva Convention?",
-    "q3":"What are the Articles of Confederation?",
-    "correct":"q2",
+    "q0":"What is the Treaty of Versailles?",
+    "q1":"What is the Geneva Convention?",
+    "q2":"What are the Articles of Confederation?",
+    "correct":"q1",
     "points":600
   },
-  "a3":{
+  "a2":{
     "answer":"This country's 2016 departure from the European Union was referred to in the media as /'Brexit/'.",
-    "q1":"What is Belgium?",
-    "q2":"What is Portugal?",
-    "q3":"What is the United Kingdom?",
-    "correct":"q3",
+    "q0":"What is Belgium?",
+    "q1":"What is Portugal?",
+    "q2":"What is the United Kingdom?",
+    "correct":"q2",
     "points":1000
   }
 };
